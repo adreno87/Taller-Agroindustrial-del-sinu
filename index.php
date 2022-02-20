96b30886ad2d87b03121b4537f70eaec41b83162
Adreno87 <adreno87xd@gmail.com>
2022-02-20 17:22:48 -0500

    Inicio

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="es">
+<?php require_once("partes/HEAD.php"); ?>
+
+<body class="hold-transition login-page">
+
+<div class="login-box">
+    <div class="login-logo">
+    <b>Admin</b>LTE</a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="card">
+        <div class="card-body login-card-body">
+        <p class="login-box-msg">Inicio de Sesion</p>
+
+        <form action="" method="post">
+            <div class="input-group mb-3">
+            <input type="email" name="email" id="email" class="form-control" placeholder="Email">
+            <div class="input-group-append">
+                <div class="input-group-text">
+                <span class="fas fa-envelope"></span>
+                </div>
+            </div>
+            </div>
+            <div class="input-group mb-3">
+            <input type="password" name="clave" id="clave" class="form-control" placeholder="Contraseña">
+            <div class="input-group-append">
+                <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+                </div>
+            </div>
+            </div>
+            <div class="row">
+            <div class="col-8">
+                <div class="icheck-primary">
+                <input type="checkbox" id="remember">
+                <label for="remember">
+                    Recuardame
+                </label>
+                </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-4">
+                <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+            </div>
+            <!-- /.col -->
+            </div>
+        </form>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php require_once("partes/scripts.php"); ?>
+
+</body>
+</html>
---
+++ plantilla.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<?php require_once("partes/HEAD.php"); ?>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+
+<?php require_once("partes/header.php"); ?>
+<?php require_once("partes/nav.php"); ?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper"> 
+
+<?php require_once("modulos/plantilla.php"); ?>
+
+</div>
+    
+
+
+
+
+
+<?php require_once("partes/scripts.php"); ?>
+
+</body>
+</html>
