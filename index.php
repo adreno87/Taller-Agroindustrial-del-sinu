897e8209ffa234f3bbafae9b24d63da0d862d078
Adreno87 <adreno87xd@gmail.com>
2022-02-21 14:26:30 -0500

    colocacion logo y cambio de input

--- index.php
+++ index.php
@@ -3,12 +3,26 @@
 <?php require_once("partes/HEAD.php"); ?>
 
 
-
+<!-- <style>
+    .logofoto{
+  width: 10%;
+  height: 10%;
+  position: relative;
+  top: 50%;
+  left: 30%;
+   transform: translate(-50%,0%);
+} -->
+</style>
 <body class="hold-transition login-page1">
 
 <div class="login-box">
+<div class="logofoto">
+        <img src="img/logoSINU.png" alt="">
+</div>
     <div class="login-logo">
-        <b class="colortitulo">Taller Agroindustrial del Sinu</b></a>
+        <!-- <b class="colortitulo">Taller Agroindustrial del Sinu</b> -->
+        <br>
+        
     </div>
     <!-- /.login-logo -->
     
@@ -18,11 +32,11 @@
                 <form class="box" action="" method="post">
                     <h1>Login</h1>
                     <div class="input-group mb-3">
-                        <input type="text" name="" placeholder="Username">
+                        <input type="email" name="email" placeholder="email">
                         
                     </div>
                     <div class="input-group mb-3">
-                        <input type="password" name="" placeholder="Password">
+                        <input type="password" name="ClaveS" placeholder="Contraseña">
                     </div>
                     <div class="col-mb3">
                             <input type="submit" name="" value="Login">
