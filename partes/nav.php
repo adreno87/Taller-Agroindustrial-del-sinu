027d545e2c40b9585c7f6e804c9928b07db45983
jonathan <ciclejhonatansoft@gmail.com>
2022-02-22 17:09:00 -0500

    REVISANDO PLANTILLA

--- partes/nav.php
+++ partes/nav.php
@@ -2,20 +2,20 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="index3.html" class="brand-link">
-        <img src="img/logoSinu.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Taller Agroindustrial <br>  del Sinu
+        <img src="img/logo_sinu_sena.png" alt="AdminLTE Logo" class="brand-image" style="opacity: .8">
+        <span class="brand-text font-weight-light" >Taller Agroindustrial <br>del Sinu
         </span>
         </a>
 
         <!-- Sidebar -->
-        <div class="sidebar">
+        <div class="sidebar" style="background-color:#FF9F45">
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
             <img src="img/jotaro.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-            <a href="#" class="d-block">Jotaro Kujo</a>
+            <a href="#" class="d-block">Kujo</a>
             </div>
         </div>
 
