ae8416ffe327d039e09279ab0404a1ba82a1329b
jonathan <ciclejhonatansoft@gmail.com>
2022-02-23 14:50:48 -0500

    plantilla cambios

--- modulos/plantilla.php
+++ modulos/plantilla.php
@@ -89,7 +89,7 @@
           <div class="col-md-12">
             <div class="card">
               <div class="card-header">
-                <h5 class="card-title">Monthly Recap Report</h5>
+                <h5 class="card-title">Mes de reportes</h5>
 
                 <div class="card-tools">
                   <button type="button" class="btn btn-tool" data-card-widget="collapse">
--- partes/nav.php
+++ partes/nav.php
@@ -1,10 +1,10 @@
   <!-- Main Sidebar Container -->
   <!--menu izquierdo-->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <aside style="background-color:#B5FE83"class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <a href="index3.html" class="brand-link" ><br>
+      <img src="img/logoSINU.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .9">
+      <span class="brand-text font-weight-light"style="color:black" >Taller<br>agroindustrial<br>del SINU</span>
     </a>
 
     <!-- Sidebar -->
@@ -12,10 +12,10 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block" style="color:black">Alexander Pierce</a>
         </div>
       </div>
 
