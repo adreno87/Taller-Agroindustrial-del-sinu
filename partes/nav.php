5f86d918373e8688aed77cca389e1443cbc3060f
jonathan <ciclejhonatansoft@gmail.com>
2022-02-24 18:48:41 -0500

    nav

--- partes/nav.php
+++ partes/nav.php
@@ -166,21 +166,15 @@
               <li class="nav-item">
                 <a href="pages/tables/simple.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Simple Tables</p>
+                  <p>Tabla simple</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="pages/tables/data.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>DataTables</p>
+                  <p>Tabla exel</p>
                 </a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/tables/jsgrid.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>jsGrid</p>
-                </a>
-              </li>
+              </li>              
             </ul>
           </li>
           
@@ -210,7 +204,7 @@
               <li class="nav-item">
                 <a href="pages/mailbox/read-mail.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Read</p>
+                  <p>Leer</p>
                 </a>
               </li>
             </ul>
