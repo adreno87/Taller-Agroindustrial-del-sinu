fbbf287310cdd13f76b1e4fd46f92237c701ed79
Adreno87 <adreno87xd@gmail.com>
2022-04-07 18:37:39 -0500

    update 1

--- R_Humanos.php
+++ R_Humanos.php
@@ -15,6 +15,23 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
 </div>
     
 <?php require_once("partes/scripts.php"); ?>
