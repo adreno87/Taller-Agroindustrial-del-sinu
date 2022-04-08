8ebae276874ec84af67d313d2edf9a3f096df8a0
Adreno87 <adreno87xd@gmail.com>
2022-04-07 21:22:07 -0500

    consulta y bloques

--- R_Humanos.php
+++ R_Humanos.php
@@ -16,34 +16,14 @@
         <br>
     <main role="main" class="col-md-9 ml-sm-auto col-lg-12 px-md-4">
         <h1>Recursos Humanos</h1>
+        
+        <button type="button" class="btn btn-info" value="insertar" name="Boton_insertar" >Insertar Informe</button>
+        <br><br>
+        
+
         <br>
-        <div id="formulario">
-        </div>
         <br>
-        <div id="consulta" >
-            <table class="table table-bordered bg-light" > 
-                <tr>
-                    <th>Fecha</th>
-                    <th>Descripcion</th>
-                    <th></th>
-                    <th></th>
-                </tr>
-                <?php
-                $consulta_r = "SELECT * FROM recursos_humanos order by fecha desc";
-                $resultado_r = mysqli_query($conexion, $consulta_r) or die("Error en la consulta");
-                while ($humanos = mysqli_fetch_array($resultado_r)) {
-                
-                ?>
-                <tr>
-                    <td><?php echo $humanos['fecha']; ?></td>
-                    <td><?php echo $humanos['descripcion']; ?></td>
-                    <td><button title="Editar" type="button" class="btn btn-success">Editar</button></td>
-                    <td><button title="Borrar" type="button" class="btn btn-danger">Borrar</button></td>
-                </tr>
-                <?php
-                } ?>
-            </table>
-        </div>
+        <?php require_once('bloques/r_humanos/consulta_r.php'); ?>
 
 
 
---
+++ bloques/r_humanos/consulta_r.php
@@ -0,0 +1,24 @@
+<div id="consulta" >
+            <table class="table table-bordered bg-light" > 
+                <tr>
+                    <th>Fecha</th>
+                    <th>Descripcion</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                <?php
+                $consulta_r = "SELECT * FROM recursos_humanos order by fecha desc";
+                $resultado_r = mysqli_query($conexion, $consulta_r) or die("Error en la consulta");
+                while ($humanos = mysqli_fetch_array($resultado_r)) {
+                
+                ?>
+                <tr>
+                    <td><?php echo $humanos['fecha']; ?></td>
+                    <td><?php echo $humanos['descripcion']; ?></td>
+                    <td><button title="Editar" type="button" class="btn btn-success">Editar</button></td>
+                    <td><button title="Borrar" type="button" class="btn btn-danger">Borrar</button></td>
+                </tr>
+                <?php
+                } ?>
+            </table>
+        </div>
