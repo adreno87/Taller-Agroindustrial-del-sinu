e380d0fcdd757bcd1872d10a21dc375e817920e5
Adreno87 <adreno87xd@gmail.com>
2022-04-08 22:30:14 -0500

    consulta y editar

--- bloques/r_humanos/editar_r.php
+++ bloques/r_humanos/editar_r.php
@@ -33,7 +33,6 @@
             </tr>
             <tr>
                 <th>Informe</th>
-                 <?php //echo $humanos_d[2];?> 
                 <td><input type="file" name="informe" id="informe" placeholder="Informe" class="form-control"></td>
             </tr>
             <tr>
@@ -66,33 +65,66 @@
 
 <br>
 <br>
-
-<div id="consulta" >
-            <table class="table table-bordered bg-light" > 
-                <tr>
+<section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">DataTable with minimal features & hover style</h3>
+              </div>
+              <!-- /.card-body -->
+              <div class="card-body">
+                <table id="example1" class="table table-bordered table-striped">
+                  <thead>
+                  <tr>
                     <th>Fecha</th>
                     <th>Informe</th>
                     <th>Descripcion</th>
                     <th></th>
                     <th></th>
-                </tr>
-                <?php
-                $consulta_r = "SELECT * FROM recursos_humanos order by fecha desc";
-                $resultado_r = mysqli_query($conexion, $consulta_r) or die("Error en la consulta");
-                while ($humanos = mysqli_fetch_array($resultado_r)) {
-                
-                ?>
-                <tr>
-                    <td><?php echo $humanos['fecha']; ?></td>
-                    <td><?php echo $humanos['informe']; ?></td>
-                    <td><?php echo $humanos['descripcion']; ?></td>
-                    <td><button title="Editar" type="button" class="btn btn-success">Editar</button></td>
-                    <td><button  onclick="eliminarinforme_r(<?php echo $humanos['id']; ?>)" title="Borrar" type="button" class="btn btn-danger">Borrar</button></td>
-                </tr>
-                <?php
-                } ?>
-            </table>
+                  </tr>
+                  </thead>
+                  <tbody>
+                    <?php
+                    $consulta_r = "SELECT * FROM recursos_humanos order by fecha desc";
+                    $resultado_r = mysqli_query($conexion, $consulta_r) or die("Error en la consulta");
+                    while ($humanos = mysqli_fetch_array($resultado_r)) {
+                    ?>
+                    <tr>
+                      <td><?php echo $humanos['fecha']; ?></td>
+                      <td><?php echo $humanos['informe']; ?></td>
+                      <td><?php echo $humanos['descripcion']; ?></td>
+                      <td><button onclick="window.location = 'R_Humanos.php?id=<?php echo $humanos['id']; ?>'" title="Editar" type="button" class="btn btn-success">Editar</button></td>
+                      <td><button  onclick="eliminarinforme_r(<?php echo $humanos['id']; ?>)" title="Borrar" type="button" class="btn btn-danger">Borrar</button></td>
+                    </tr>
+                    <?php
+                    } ?>
+                  </tbody>
+                  <tfoot>
+                  <tr>
+                    <th>Fecha</th>
+                    <th>Informe</th>
+                    <th>Descripcion</th>
+                    <th></th>
+                    <th></th>
+                  </tr>
+                  </tfoot>
+                </table>
+              </div>
+            </div>
+            <!-- /.card -->
+            
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
         </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container-fluid -->
+    </section>
+    
+
 
 
 <script>
