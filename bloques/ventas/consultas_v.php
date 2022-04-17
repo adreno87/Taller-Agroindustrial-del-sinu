f8fcfa0af0ddef08c37920ca184db7a2ede1c313
jefercuero <cueromorenojefer@gmail.com>
2022-04-17 11:45:47 -0500

    comienzo

---
+++ bloques/ventas/consultas_v.php
@@ -0,0 +1,68 @@
+<div id= formulario>
+<table width="100%"  border="3"  cellspacing="0">
+<tr>
+    <th>Fecha</th>
+    <th>Subtotal</th>
+    <th>Total</th>
+    <th>Impuesto</th>
+    <th>Proveedor</th>
+    <th>producto</th>
+
+    <th></th>
+    <th></th>
+    
+
+
+</tr>
+
+<?php 
+
+$consulta_v = " SELECT * FROM ventas order by fecha";
+$resultado_v = mysqli_query($conexion,$consulta_v) or die('no se consulto las ventas');
+while($venta = mysqli_fetch_array($resultado_v)){
+
+
+?>
+
+
+<tr>
+
+    <td><?php echo $venta[ 'fecha' ]; ?></td>
+    <td><?php echo $venta[ 'sub_total' ]; ?></td>
+    
+    <td><?php echo $venta[ 'total' ]; ?></td>
+    <td><?php echo $venta[ 'impuesto' ]; ?></td>
+    <td><?php echo $venta[ 'provedor' ]; ?></td>
+
+    <td>
+        
+    <?php
+
+   $consulta_p = "SELECT * FROM productos WHERE id = " .  $venta[ 'productos_id' ];
+    $res_producto =  mysqli_query($conexion,$consulta_p) or die('no consulto el producto');
+    $productos_idd =mysqli_fetch_array($res_producto);
+
+    
+    
+    ?>
+
+
+</td>
+
+    <td>  <img src="img/editar.jpg"  width="23"  title="Editar"   alt="">  </td>   
+    <td>  <img src="img/eliminar.jpg"  width="24"  title="eliminar"   alt="">          </td>
+
+
+</tr>
+
+<?php }  ?>
+</table>
+
+
+
+
+
+ </div>
+ <br>
+
+</div>
