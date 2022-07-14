5fe5d76e4cd5d4e487817b03ab02064eac7e00fc
adreno87 <42704000+adreno87@users.noreply.github.com>
2022-07-14 14:47:24 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@
     $servidor="localhost";
     $usuario="root";
     $clave="";
-    $bd="sinu";
+    $bd="bd_proyecto_sinu";
 
     $conexion=mysqli_connect($servidor,$usuario,$clave) or die("Error al conectar");
     mysqli_select_db($conexion,$bd) or die("Error al conectar la base de datos");
