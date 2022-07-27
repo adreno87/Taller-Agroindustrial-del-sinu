7501e96ddbd295402198d28c66cc418c586823f9
jonathan <ciclejhonatansoft@gmail.com>
2022-07-27 14:46:08 -0500

    borrar alt linea 61 de consulta_tc

--- bloques/tecnologia/consulta_tc.php
+++ bloques/tecnologia/consulta_tc.php
@@ -58,7 +58,7 @@
                       <td><button onclick="window.location = 'tecnologia.php?id=<?php echo $tecnologia['id']; ?>'" title="Editar" type="button" class="btn btn-success">
                       <i class="fas fa-pencil-alt"></i>Editar</button></td>
                       
-                      <td><button  onclick="eliminartecnologia(<?php echo $tecnologia['id']; ?>);" title="Borrar" type="button" class="btn btn-danger" alt=""> <i class="fas fa-trash"></i>Borrar</button></td>
+                      <td><button  onclick="eliminartecnologia(<?php echo $tecnologia['id']; ?>);" title="Borrar" type="button" class="btn btn-danger" > <i class="fas fa-trash"></i>Borrar</button></td>
                     </tr>
                     <?php
                     } ?>
