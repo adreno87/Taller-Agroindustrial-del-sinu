06d474f65616c09f54e452f55da9b946af37a0c4
jefercuero <cueromorenojefer@gmail.com>
2022-07-30 13:25:49 -0500

    ventas

---
+++ Ventas.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+    <html lang="es">
+    <?php require_once("partes/HEAD.php"); ?>
+    <?php require_once("bloqueo.php"); ?>
+    <?php require_once("conexion.php"); ?>
+
+
+    <body class="hold-transition sidebar-mini layout-fixed">
+
+    <?php require_once("partes/header.php"); ?>
+    <?php require_once("partes/nav.php"); ?>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper"> 
+    <!-- <div class="breadcrumb-item active"> -->
+        <br>
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-12 px-md-4">
+        <h1>Ventas</h1>
+        
+ 
+        <?php require_once('bloques/ventas/formulario_v.php'); ?>
+
+        <br>
+        <br>
+        
+
+        <?php require_once('bloques/ventas/consultas_v.php'); ?>
+
+
+
+     
+
+
+
+
+
+
+    </main>
+
+    <!-- Content Wrapper. Contains page content -->
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+    </div>
+        
+   
+    </body>
+
+    <script src="js/funciones.js"></script>
+    <?php require_once("partes/scripts.php");?>
+    
+
+    </html>
